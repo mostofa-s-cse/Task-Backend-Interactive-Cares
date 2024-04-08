9ac3291a4f21b599f9e5d0ef2371a9e0a6984116
mostofa-s-cse <mostofa.s.cse@gmail.com>
2024-04-08 16:36:15 +0600

    readme file update

--- routes/users/index.js
+++ routes/users/index.js
@@ -10,15 +10,15 @@ router.post("/signup", user.signup);
 router.post("/login", user.login);
 
 //http://localhost:5000/api/v1/user/
-router.get("/", verifyToken, authorization("super_admin", "admin"), user.getAllUsers);
+router.get("/", verifyToken, authorization("super_admin", "admin","user"), user.getAllUsers);
 
 //http://localhost:5000/api/v1/user/:id
-router.get("/:id", verifyToken, authorization("super_admin", "admin"), user.getSingleUser);
+router.get("/:id", verifyToken, authorization("super_admin", "admin","user"), user.getSingleUser);
 
 //http://localhost:5000/api/v1/user/update/:id 
-router.put("/:id", verifyToken, authorization("super_admin", "admin"), user.updateUser);
+router.put("/:id", verifyToken, authorization("super_admin", "admin","user"), user.updateUser);
 
 // //http://localhost:5000/api/v1/user/delete/:id
-router.delete("/:id", verifyToken, authorization("super_admin", "admin"), user.deleteUser);
+router.delete("/:id", verifyToken, authorization("super_admin", "admin","user"), user.deleteUser);
 
 module.exports = router;
